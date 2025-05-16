46f017488b73b95095b68e3a303a7c66d4a2a498
Juan M Quijano <juanquijano1352@gmail.com>
2025-05-16 01:02:25 -0500

    Fix typo in environment variable reference in backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket  = var.terraform_state_bucket
-    key     = "${var.environmnet}/terraform.tfstate"
+    key     = "${var.environment}/terraform.tfstate"
     region  = var.aws_region
     encrypt = true
   }
